34202805582b1e87a87aeab89a00b4a066f16d25
Mats Olsberg <mats.olsberg@evry.com>
2020-03-07 18:59:31 +0100

    Add function and favicon

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
 
 import '@/assets/styles/main.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
